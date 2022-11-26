9bfbd207196ae58432cd883655adb33b190204d9
JBQGlobal <tecnologia@jbq.global>
2022-11-26 15:47:29 -0300

    feat: criando função no service para criar usuario

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserRole } from './user-roles.enum';
+import { User } from './user.entity';
 import { UserRepository } from './users.repository';
 
 @Injectable()
@@ -7,8 +10,16 @@ export class UsersService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
-  ) {}
+  ) { }
 
+  async createUserAdmin(createUserDTO: CreateUserDto): Promise<User> {
+    if (createUserDTO.password != createUserDTO.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
 
+    }
+    else {
+      return this.userRepository.createUser(createUserDTO, UserRole.ADMIN);
+    }
+  }
 
 }
