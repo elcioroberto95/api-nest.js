d53c5e23c386cbb82cb9fa1c2ab797f417917c98
JBQGlobal <tecnologia@jbq.global>
2022-11-26 21:32:01 -0300

    feat: creating signUp function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,15 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
-    
+    @Post('/signup')
+    async signUp(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<{ message: string }> {
+
+        await this.authService.signUp(createUserDto);
+
+        return {
+            message: 'Cadastro realizado com sucesso',
+        }
+    }
 }
 
 
