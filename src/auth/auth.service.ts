4e63ade168e0fec2276ec45aafe7f70dcc85d5fd
JBQGlobal <tecnologia@jbq.global>
2022-11-26 21:32:37 -0300

    feat: creating auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { UserRepository } from '../users/users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../users/dtos/create-user.dto'
+import { User } from '../users/user.entity';
+import { UserRole } from '../users/user-roles.enum';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository,
+    ) { }
+
+    async signUp(createUserDto: CreateUserDto): Promise<User> {
+        if (createUserDto.password != createUserDto.passwordConfirmation) {
+            throw new UnprocessableEntityException('As senhas não conferem');
+        } else {
+            return await this.userRepository.createUser(createUserDto, UserRole.USER);
+        }
+    }
+}
